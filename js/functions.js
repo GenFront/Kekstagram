d9e48a3f02bac6147ec595dcb4a74bfb84531e59
GenFront <genti7042@gmail.com>
2026-05-09 11:59:03 +0300

    Задание module2-task1

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+
+
+// function isLengthValid(str, maxLength) {
+//   if(str.length <= maxLength) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// isLengthValid('Text', 10);
+
+
