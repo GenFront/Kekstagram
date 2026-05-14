724d64ffd1398bbf58dfe666336d218b328b4d61
GenFront <genti7042@gmail.com>
2026-05-14 09:41:32 +0300

    Выполнено первое д/з несколькими способами

--- js/functions.js
+++ js/functions.js
@@ -1,17 +1,20 @@
 
 
-// function isLengthValid(string, maxLength) {
-//   if(string.length <= maxLength) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// isLengthValid('Text', 10);
-
-// или стрелочная функция
-
-const isLengthValid = (string, maxLength) => string.length <= maxLength;
-
+function isLengthValid(str, maxLength) {
+  if(str.length <= maxLength) {
+    return true;
+  } else {
+    return false;
+  }
+}
 isLengthValid('Text', 10);
+
+// или стрелочная функция:
+// const isLengthValid = (string, maxLength) => string.length <= maxLength;
+
+//или через тернарник:
+//function isLengthValid(str, maxLength) {
+// const isValid = string.length <= maxLength;
+// return isValid;
+//}
 
