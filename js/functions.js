3430e2c6d68dfe35f050144e43fe501d255e0de6
GenFront <genti7042@gmail.com>
2026-05-14 16:02:24 +0300

    Выполнено task2

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,46 @@
+//task1
 
+// Строка короче 20 символов
+isLengthValid('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+isLengthValid('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+isLengthValid('проверяемая строка', 10); // false
 
 function isLengthValid(str, maxLength) {
   if(str.length <= maxLength) {
     return true;
-  } else {
-    return false;
   }
 }
+
 isLengthValid('Text', 10);
 
 // или стрелочная функция:
-// const isLengthValid = (string, maxLength) => string.length <= maxLength;
+// const isLengthValid=(string, maxLength)=> string.length <= maxLength;
 
 //или через тернарник:
-//function isLengthValid(str, maxLength) {
-// const isValid = string.length <= maxLength;
+// function isLengthValid(str, maxLength) {
+// const isValid=string.length <=maxLength;
 // return isValid;
 //}
 
+
+//task2
+
+function isPalindrome(string) {
+  const normalizedString = string.toLowerCase().replaceAll(' ', '');
+  let emptyString = '';
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    emptyString += normalizedString.at(i);
+  }
+  return emptyString === normalizedString;
+
+}
+
+
+// Строка является палиндромом
+isPalindrome('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+isPalindrome('ДовОд'); // true
+// Это не палиндром
+isPalindrome('Кекс'); // false
